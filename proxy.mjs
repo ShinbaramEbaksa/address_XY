a9cd188d72e7a5a0683805399402b7f469ed7d5c
ShinbaramEbaksa <vousetmonetoile@gmail.com>
2024-06-05 13:35:33 +0900

    ㅁ

--- proxy.mjs
+++ proxy.mjs
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 const app = express();
 
-const API_KEY = 'YOUR_API_KEY';
+const API_KEY = '6AF229F1-482F-3323-942F-37D854F22E2A';
 const API_URL = 'https://api.vworld.kr/req/address?service=address&request=getcoord&version=2.0&crs=epsg:4326';
 
 app.use((req, res, next) => {
@@ -16,9 +16,10 @@ app.use((req, res, next) => {
 app.get('/address', async (req, res) => {
     try {
         const address = req.query.address;
-        const response = await fetch(`${API_URL}&address=${encodeURIComponent(address)}&refine=true&simple=false&format=json&type=parcel&key=${API_KEY}`);
-        const data = await response.json();
-        res.json(data);
+        const response = await fetch(`${API_URL}&address=${encodeURIComponent(address)}&refine=true&simple=false&format=xml&type=road&key=${API_KEY}`);
+        const data = await response.text(); // XML 형식이므로 .text() 사용
+        res.header('Content-Type', 'application/xml');
+        res.send(data);
     } catch (error) {
         res.status(500).send('Internal Server Error');
     }
